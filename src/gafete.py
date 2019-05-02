5bb7ea3eb078a6afbc9882a9e341f1c1a87c1a94
Steven <sasilva1998@gmail.com>
2019-05-01 23:44:40 -0500

    codigo inicial para el uso

---
+++ src/gafete.py
@@ -0,0 +1,34 @@
+# test of printing multiple fonts to the ILI9341 on an M5Stack using H/W SP
+# MIT License; Copyright (c) 2017 Jeffrey N. Magee
+
+from ili934xnew import ILI9341, color565
+from machine import Pin, SPI
+import tt14
+import glcdfont
+import tt14
+import tt24
+import tt32
+import tt36
+
+fonts = [glcdfont,tt14,tt24,tt32,tt36]
+
+text = 'Now.'
+
+spi = SPI(miso=Pin(12), mosi=Pin(13, Pin.OUT), sck=Pin(14, Pin.OUT))
+
+display = ILI9341(
+    spi,
+    cs=Pin(15),
+    dc=Pin(5),
+    rst=Pin(4),
+    width=240,
+    height=320,
+    mirror=False)
+display.erase()
+display.set_pos(0,0)
+
+display.set_font(fonts[4])
+display.print("Steven Silva")
+display.set_font(fonts[2])
+display.print(" ")
+display.print("FunPython Member")
